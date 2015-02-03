3b6f27999ffaaf2a328ab6943047edb19f1727e4
Yuan Zhang <yuanzh@csail.mit.edu>
2015-02-03 17:16:47 -0500

    minor fix

--- Release/run_chinese_test.sh
+++ Release/run_chinese_test.sh
@@ -2,7 +2,7 @@ runid=$1
 
 ln -s ctb.seg.test ../data/ctb.test.$runid
 
-./SegParser model-name:../runs/ctb.model.$runid test test-file:../data/ctb.test.$runid output-file:../runs/ctb.out.$runid seed:14 evalpunc:false test-converge:300 $@
+./SegParser model-name:../runs/ctb.model.$runid test test-file:../data/ctb.test.$runid output-file:../runs/ctb.out.$runid seed:14 evalpunc:false test-converge:300 devthread:10 $@
 
     rm ../data/ctb.test.$runid
 
