d23faa684d671ed947b794a2997a56acc28bba24
Yuan Zhang <yuanzh@csail.mit.edu>
2015-02-11 15:08:27 -0500

    minor change

--- Release/run_chinese.sh
+++ Release/run_chinese.sh
@@ -3,7 +3,7 @@ runid=$1
 ln -s ctb.seg.train ../data/ctb.train.$runid
 ln -s ctb.seg.dev ../data/ctb.test.$runid
 
-./SegParser train test train-file:../data/ctb.train.$runid model-name:../runs/ctb.model.$runid test-file:../data/ctb.test.$runid output-file:../runs/ctb.out.$runid seed:14 earlystop:40 evalpunc:false C:0.01 train-converge:300 test-converge:300 $@ | tee ../runs/ctb.log.$runid
+./SegParser train test train-file:../data/ctb.train.$runid model-name:../runs/ctb.model.$runid test-file:../data/ctb.test.$runid output-file:../runs/ctb.out.$runid seed:14 earlystop:40 evalpunc:false C:0.001 train-converge:300 test-converge:300 $@ | tee ../runs/ctb.log.$runid
 
     rm ../data/ctb.train.$runid
     rm ../data/ctb.test.$runid
